b812cf47c14057407d095c7b1b9976f6b384bbfa
BadescuTudor <121316745+Tudor893@users.noreply.github.com>
2024-08-01 23:31:04 +0300

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -173,6 +173,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+      
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
